e001ee6f3d56c5b6603e501effc763437a3ae9d8
mahmoudashry98 <mahmoud.3ashry5555@gmail.com>
2022-01-15 04:57:22 +0200

    create Ui comment

--- lib/modules/social_app/settings/settings_screen.dart
+++ lib/modules/social_app/settings/settings_screen.dart
@@ -1,220 +1,248 @@
-import 'package:conditional_builder/conditional_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:scoial_app/layout/social_app/cubit/cubit.dart';
 import 'package:scoial_app/layout/social_app/cubit/states.dart';
-import 'package:scoial_app/modules/social_app/edit_profile/edit_profile_screen.dart';
+import 'package:scoial_app/modules/social_app/social_register/cubit/cubit.dart';
 import 'package:scoial_app/shared/components/components.dart';
-import 'package:scoial_app/shared/components/constants.dart';
-import 'package:scoial_app/shared/style/color/color.dart';
 
 // ignore: must_be_immutable
 class SettingsScreen extends StatelessWidget {
   var nameController = TextEditingController();
-  var imageController = TextEditingController();
-  var emailController = TextEditingController();
   var bioController = TextEditingController();
+  var phoneController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<SocialCubit, SocialStates>(
       listener: (context, state) {},
       builder: (context, state) {
         var userModel = SocialCubit.get(context).userModel;
-        nameController.text =userModel.name;
-        imageController.text =userModel.image;
-        bioController.text =userModel.bio;
-        return ConditionalBuilder(
-         condition: SocialCubit.get(context).userModel != null ,
-          builder: (context) => Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Container(
-                  height: 200.0,
-                  child: Stack(
-                    alignment: AlignmentDirectional.bottomCenter,
-                    children: [
-                      Align(
-                        alignment: AlignmentDirectional.topCenter,
-                        child: Container(
-                          width: double.infinity,
-                          height: 140.0,
+        var profileImage = SocialRegisterCubit.get(context).profileImage;
+        var coverImage = SocialCubit.get(context).coverImage;
+        nameController.text = userModel.name;
+        bioController.text = userModel.bio;
+        phoneController.text = userModel.phone;
+
+        return Scaffold(
+          ///appbar , update button
+          appBar: defaultAppBar(
+            context: context,
+            title: 'Edit Profile',
+            actions: [
+              defaultTextButton(
+                function: () {
+                  SocialCubit.get(context).updateUser(
+                    name: nameController.text,
+                    phone: phoneController.text,
+                    bio: bioController.text,
+                  );
+                },
+                text: 'Update',
+              ),
+              SizedBox(
+                width: 15.0,
+              ),
+            ],
+          ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  if (state is SocialUserUpdateLoadingState)
+                    LinearProgressIndicator(),
+                  if (state is SocialUserUpdateLoadingState)
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                  Container(
+                    height: 180.0,
+                    child: Stack(
+                      alignment: AlignmentDirectional.bottomCenter,
+                      children: [
+                        Align(
+                          alignment: AlignmentDirectional.topCenter,
+                          ///ProfileImage,CoverImage,and IconButtonCamera
+                          child: Stack(
+                            alignment: AlignmentDirectional.topEnd,
+                            children: [
+                              Container(
+                                width: double.infinity,
+                                height: 140.0,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(4.0),
+                                      topRight: Radius.circular(4.0)),
+                                  image: DecorationImage(
+                                    image: coverImage == null
+                                        ? NetworkImage('${userModel.cover}')
+                                        : FileImage(coverImage),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              IconButton(
+                                icon: CircleAvatar(
+                                  radius: 20.0,
+                                  child: Icon(
+                                    Icons.camera_alt,
+                                    size: 16.0,
+                                  ),
+                                ),
+                                onPressed: () {
+                                  SocialCubit.get(context).getCoverImage();
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
                           decoration: BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(4.0),
-                                topRight: Radius.circular(4.0)),
-                            image: DecorationImage(
-                              image: NetworkImage('${userModel.cover}'),
-                              fit: BoxFit.cover,
+                            shape: BoxShape.circle,
+                            color: Theme.of(context).scaffoldBackgroundColor,
+                          ),
+                          child: Stack(
+                            alignment: AlignmentDirectional.bottomEnd,
+                            children: [
+                              CircleAvatar(
+                                radius: 62.0,
+                                backgroundColor:
+                                    Theme.of(context).scaffoldBackgroundColor,
+                                child: CircleAvatar(
+                                  radius: 60.0,
+                                  backgroundImage: profileImage == null
+                                      ? NetworkImage('${userModel.image}')
+                                      : FileImage(profileImage),
+                                ),
+                              ),
+                              IconButton(
+                                icon: CircleAvatar(
+                                  radius: 20.0,
+                                  child: Icon(
+                                    Icons.camera_alt,
+                                    size: 16.0,
+                                  ),
+                                ),
+                                onPressed: () {
+                                  SocialRegisterCubit.get(context).getProfileImage();
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  if (SocialRegisterCubit.get(context).profileImage != null ||
+                      SocialCubit.get(context).coverImage != null)
+                    Row(
+                      children: [
+                        if (SocialRegisterCubit.get(context).profileImage != null)
+                          Expanded(
+                            child: Column(
+                              children: [
+                                defaultButton(
+                                  function: () {
+                                    SocialCubit.get(context).uploadProfileImage(
+                                      name: nameController.text,
+                                      phone: phoneController.text,
+                                      bio: bioController.text,
+                                    );
+                                  },
+                                  text: 'upload profile',
+                                ),
+                                if (state is SocialUserUpdateLoadingState)
+                                  SizedBox(
+                                    height: 5.0,
+                                  ),
+                                if (state is SocialUserUpdateLoadingState)
+                                  LinearProgressIndicator(),
+                              ],
                             ),
                           ),
+                        SizedBox(
+                          width: 5.0,
                         ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Theme.of(context).scaffoldBackgroundColor,
-                        ),
-                        child: CircleAvatar(
-                          radius: 62.0,
-                          backgroundColor:
-                          Theme.of(context).scaffoldBackgroundColor,
-                          child: CircleAvatar(
-                            radius: 60.0,
-                            backgroundImage: NetworkImage('${userModel.image}'),
+                        if (SocialCubit.get(context).coverImage != null)
+                          Expanded(
+                            child: Column(
+                              children: [
+                                defaultButton(
+                                  function: () {
+                                    SocialCubit.get(context).uploadCoverImage(
+                                      name: nameController.text,
+                                      phone: phoneController.text,
+                                      bio: bioController.text,
+                                    );
+                                  },
+                                  text: 'upload cover',
+                                ),
+                                if (state is SocialUserUpdateLoadingState)
+                                  SizedBox(
+                                    height: 5.0,
+                                  ),
+                                if (state is SocialUserUpdateLoadingState)
+                                  LinearProgressIndicator(),
+                              ],
+                            ),
                           ),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
+                  SizedBox(
+                    height: 10.0,
                   ),
-                ),
-                SizedBox(
-                  height: 5.0,
-                ),
-                Text(
-                  '${nameController.text}',
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-                SizedBox(
-                  height: 5.0,
-                ),
-                Text(
-                  '${bioController.text}',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 15.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: InkWell(
-                          child: Column(
-                            children: [
-                              Text(
-                                '100',
-                                style: Theme.of(context).textTheme.bodyText1,
-                              ),
-                              SizedBox(
-                                height: 5.0,
-                              ),
-                              Text(
-                                'Posts',
-                                style: Theme.of(context).textTheme.caption,
-                              ),
-                            ],
-                          ),
-                          onTap: () {},
-                        ),
-                      ),
-                      Expanded(
-                        child: InkWell(
-                          child: Column(
-                            children: [
-                              Text(
-                                '365',
-                                style: Theme.of(context).textTheme.bodyText1,
-                              ),
-                              SizedBox(
-                                height: 5.0,
-                              ),
-                              Text(
-                                'Photos',
-                                style: Theme.of(context).textTheme.caption,
-                              ),
-                            ],
-                          ),
-                          onTap: () {},
-                        ),
-                      ),
-                      Expanded(
-                        child: InkWell(
-                          child: Column(
-                            children: [
-                              Text(
-                                '10K',
-                                style: Theme.of(context).textTheme.bodyText1,
-                              ),
-                              SizedBox(
-                                height: 5.0,
-                              ),
-                              Text(
-                                'Followers',
-                                style: Theme.of(context).textTheme.caption,
-                              ),
-                            ],
-                          ),
-                          onTap: () {},
-                        ),
-                      ),
-                      Expanded(
-                        child: InkWell(
-                          child: Column(
-                            children: [
-                              Text(
-                                '250',
-                                style: Theme.of(context).textTheme.bodyText1,
-                              ),
-                              SizedBox(
-                                height: 5.0,
-                              ),
-                              Text(
-                                'Following',
-                                style: Theme.of(context).textTheme.caption,
-                              ),
-                            ],
-                          ),
-                          onTap: () {},
-                        ),
-                      ),
-                    ],
+                  defaultFormField(
+                    controller: nameController,
+                    type: TextInputType.name,
+                    validate: (String value) {
+                      if (value.isEmpty) {
+                        return 'name must not be empty';
+                      }
+                      return null;
+                    },
+                    label: 'Name',
+                    prefix: Icons.person_rounded,
                   ),
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: OutlinedButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Add Photos',
-                          style: TextStyle(
-                            color: defaultColor,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    OutlinedButton(
-                      onPressed: () {
-                        navigateTo(context, EditProfileScreen());
-                      },
-                      child: Icon(
-                        Icons.edit,
-                        size: 16.0,
-                        color: defaultColor,
-                      ),
-                    ),
-
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    OutlinedButton(
-                      onPressed: () {
-                        signOut(context);
-                      },
-                      child: Icon(
-                        Icons.logout,
-                        size: 16.0,
-                        color: defaultColor,
-                      ),
-                    )
-                  ],
-                ),
-              ],
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  defaultFormField(
+                    controller: bioController,
+                    type: TextInputType.text,
+                    validate: (String value) {
+                      if (value.isEmpty) {
+                        return 'bio must not be empty';
+                      }
+                      return null;
+                    },
+                    label: 'Bio',
+                    prefix: Icons.info_outline,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  defaultFormField(
+                    controller: phoneController,
+                    type: TextInputType.number,
+                    validate: (String value) {
+                      if (value.isEmpty) {
+                        return 'phone number must not be empty';
+                      }
+                      return null;
+                    },
+                    label: 'Phone',
+                    prefix: Icons.call,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                ],
+              ),
             ),
           ),
-          fallback: (context) => Center(child: CircularProgressIndicator()),
         );
       },
     );
